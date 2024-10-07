b52268ec2900481ec1180a0845aa02f0d3f18b47
Ebenezer Akpas <akpasebenezer0@gmail.com>
2024-10-07 01:49:52 +0100

    Update secret

--- Stylus-Bot/adminControls/adminCodes.py
+++ Stylus-Bot/adminControls/adminCodes.py
@@ -7,7 +7,7 @@ import os
 
 ############### Admin Codes ###############
 fauna_secret = "fnAFAZYyLKAAUY3-ZL5ntrRfZBPfifwOvF-EUD8v"
-CLIENT = FaunaClient(secret=fauna_secret, domain="db.us.fauna.com")
+CLIENT = FaunaClient(fauna_secret, "db.us.fauna.com")
 messenger = WhatsApp(token ="EAAXKtiII4zwBO9cZCav9yZC62kWbPJTs2djodZAWv2TthAYeTROIuOnphZAgEzr5VIlnXEeZBULgnnUW5p5u68EJ0TD8ZCtf2vZBgdlRKRpjSFYBrdtJpdjvvXmFXRFJy5uOuniqDnAVukAC8dHWikpNRyFOuGi0JTVnBKm4o0NRZBsnEBDVMSXXEIaJEsOoOsORZBPgI5PtYayX7FrxJM9oQMxwFM8QZD", phone_number_id="257257560810302") #You can use any random characters in here
 TOKEN = os.getenv("TOKEN") #application secret here
 ETHERS = WEBPY()
